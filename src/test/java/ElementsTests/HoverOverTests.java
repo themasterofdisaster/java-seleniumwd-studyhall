de08a338c999e112edf2f66e4b43d67e6e6cf721
mmagana <mmagana@switchfly.com>
2021-04-21 23:18:28 -0700

    Added testHoverOverUser2

--- src/test/java/ElementsTests/HoverOverTests.java
+++ src/test/java/ElementsTests/HoverOverTests.java
@@ -8,15 +8,29 @@ import static org.testng.Assert.*;
 
 public class HoverOverTests extends BaseTests {
 
+    private HoversPage hoversPage;
+    private HoversPage.FigureCaption caption;
+
     @Test
     public void testHoverOverUser1(){
-        HoversPage hoversPage = homePage.clickHoversLink();
-        HoversPage.FigureCaption caption = hoversPage.hoverOverFigure(1);
+        hoversPage = homePage.clickHoversLink();
+        caption = hoversPage.hoverOverFigure(1);
 
         assertTrue(caption.isCaptionDisplayed(), "Caption not displayed");
         assertEquals(caption.getFigureName(),"name: user1", "Figure name is incorrect");
         assertEquals(caption.getFigureLinkText(), "View profile", "Figure link text is incorrect");
         assertTrue(caption.getFigureLink().endsWith("/users/1"), "Link incorrect");
+
+    }
+
+    @Test
+    public void testHoverOverUser2(){
+        caption = hoversPage.hoverOverFigure(2);
+
+        assertTrue(caption.isCaptionDisplayed(), "Caption not displayed");
+        assertEquals(caption.getFigureName(),"name: user2", "Figure name is incorrect");
+        assertEquals(caption.getFigureLinkText(), "View profile", "Figure link text is incorrect");
+        assertTrue(caption.getFigureLink().endsWith("/users/2"), "Link incorrect");
 
     }
 }
