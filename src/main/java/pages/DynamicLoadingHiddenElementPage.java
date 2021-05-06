e68d8d28cc115c8b0885654418e2a14020ce6b8b
mmagana <mmagana@switchfly.com>
2021-05-05 23:22:12 -0700

    Added explicitWaitTest

--- src/main/java/pages/DynamicLoadingHiddenElementPage.java
+++ src/main/java/pages/DynamicLoadingHiddenElementPage.java
@@ -2,17 +2,27 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DynamicLoadingHiddenElementPage {
 
     WebDriver driver;
-    By startButton = By.id("start");
+    By startButton = By.cssSelector("#start button");
+    By loadingIndicator = By.id("loading");
+    By loadedText = By.id("finish");
 
     public DynamicLoadingHiddenElementPage(WebDriver driver){
         this.driver = driver;
     }
 
-    public void clickStartButton(){
+    public void clickStartButtonExplicitWait(){
         driver.findElement(startButton).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(loadingIndicator)));
+    }
+
+    public String getLoadedTest(){
+        return  driver.findElement(loadedText).getText();
     }
 }
--- src/test/java/seleniumwd/ElementsTests/WaitTests.java
+++ src/test/java/seleniumwd/ElementsTests/WaitTests.java
@@ -5,7 +5,10 @@
 
             This indicates that any time WebDriver needs to interact with an element, then it should poll the website
             for up to 30 seconds until it finds that element.
-            You can see this applied in BaseTests.java in the @BeforeClass, called SetUp (it is commented out).
+            You can see this applied in BaseTests.java in the @BeforeClass, called SetUp (it is commented out). It is
+            set up as follows:
+
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
         Explicit Wait
 
@@ -16,5 +19,18 @@
 
 package seleniumwd.ElementsTests;
 
-public class WaitTests {
+import org.testng.annotations.Test;
+import pages.DynamicLoadingHiddenElementPage;
+import seleniumwd.base.BaseTests;
+
+import static org.testng.Assert.*;
+
+public class WaitTests extends BaseTests {
+
+    @Test
+    public void explicitWaitTest(){
+        DynamicLoadingHiddenElementPage loadingPage = homePage.goToDynamicLoadingHiddenElementPage();
+        loadingPage.clickStartButtonExplicitWait();
+        assertEquals(loadingPage.getLoadedTest(), "Hello World!", "Loaded text is incorrect");
+    }
 }
