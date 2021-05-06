7b8813cfe67b69066c125745a1fd1f633e4c918c
mmagana <mmagana@switchfly.com>
2021-05-06 09:40:31 -0700

    Added fluent wait example and description

--- src/main/java/pages/DynamicLoadingHiddenElementPage.java
+++ src/main/java/pages/DynamicLoadingHiddenElementPage.java
@@ -1,9 +1,13 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class DynamicLoadingHiddenElementPage {
 
@@ -18,8 +22,22 @@ public class DynamicLoadingHiddenElementPage {
 
     public void clickStartButtonExplicitWait(){
         driver.findElement(startButton).click();
+
+        /* EXPLICIT WAIT */
         WebDriverWait wait = new WebDriverWait(driver, 5);
         wait.until(ExpectedConditions.invisibilityOf(driver.findElement(loadingIndicator)));
+    }
+
+    public void clickStartButtonFluentWait(){
+        driver.findElement(startButton).click();
+
+        /* FLUENT WAIT */
+        FluentWait wait = new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class);
+        wait.until(ExpectedConditions.invisibilityOf(
+                driver.findElement(loadingIndicator)));
     }
 
     public String getLoadedTest(){
--- src/test/java/seleniumwd/ElementsTests/WaitTests.java
+++ src/test/java/seleniumwd/ElementsTests/WaitTests.java
@@ -3,8 +3,13 @@
 
         Implicit Wait
 
-            This indicates that any time WebDriver needs to interact with an element, then it should poll the website
-            for up to 30 seconds until it finds that element.
+            The Implicit Wait in Selenium is used to tell the web driver to wait for a certain amount of time before it
+            throws a "No Such Element Exception". This indicates that any time WebDriver needs to interact with an element,
+            then it should poll the website for up to 30 seconds until it finds that element.
+
+            The default setting is 0. Once we set the time, the web driver will wait for the element for that time
+            before throwing an exception.
+
             You can see this applied in BaseTests.java in the @BeforeClass, called SetUp (it is commented out). It is
             set up as follows:
 
@@ -12,8 +17,18 @@
 
         Explicit Wait
 
-            When we use explicit waits, we use them only when we need to.
+            The Explicit Wait in Selenium is used to tell the Web Driver to wait for certain conditions (Expected Conditions)
+            or maximum time exceeded before throwing "ElementNotVisibleException" exception. It is an intelligent kind
+            of wait, but it can be applied only for specified elements. It gives better options than implicit wait as it
+            waits for dynamically loaded Ajax elements. When we use explicit waits, we use them only when we need to.
 
+        Fluent Wait
+
+            The Fluent Wait in Selenium is used to define maximum time for the web driver to wait for a condition, as
+            well as the frequency with which we want to check the condition before throwing an "ElementNotVisibleException"
+            exception. It checks for the web element at regular intervals until the object is found or timeout happens.
+
+            Frequency: Setting up a repeat cycle with the time frame to verify/check the condition at the regular interval of time
 
  */
 
@@ -30,7 +45,15 @@ public class WaitTests extends BaseTests {
     @Test
     public void explicitWaitTest(){
         DynamicLoadingHiddenElementPage loadingPage = homePage.goToDynamicLoadingHiddenElementPage();
-        loadingPage.clickStartButtonExplicitWait();
+        loadingPage.clickStartButtonExplicitWait(); // this method implements an explicit wait.
         assertEquals(loadingPage.getLoadedTest(), "Hello World!", "Loaded text is incorrect");
     }
+
+    @Test
+    public void fluentWaitTest(){
+        DynamicLoadingHiddenElementPage loadingPage = homePage.goToDynamicLoadingHiddenElementPage();
+        loadingPage.clickStartButtonFluentWait(); // this method implements a fluent wait.
+        assertEquals(loadingPage.getLoadedTest(), "Hello World!", "Loaded text is incorrect");
+    }
+
 }
